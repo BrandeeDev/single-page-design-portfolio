ba4a1293cf5977ddf710375cafb7430683962d5a
Hazem Gader <hgader@euro.adroot>
2022-10-25 09:46:16 +0200

    overflow correction

--- src/UI/Main.tsx
+++ src/UI/Main.tsx
@@ -1,7 +1,7 @@
 
 const Main=(props: any)=>{
     return(
-        <main className=" appearance-none min-h-screen  bg-[#FFF7F0] px-4  pt-4  sm:px-[39px] sm:pt-[34px] lg:px-[164px]">
+        <main className=" min-h-screen overflow-hidden  px-4  bg-[#FFF7F0] pt-4  sm:px-[39px] sm:pt-[34px] lg:px-[164px]">
                 {props.children}
         </main>
     );
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -43,9 +43,9 @@ const Work=()=>{
         <div className="flex flex-col justify-center items-center my-[100px] sm:my-[120px] lg:my-[88px] " >
             <h3 className="mt-8 font-plus_jakarta_s font-bold  not-italic text-2xl leading-[30px] text-center text-[#030303] sm:text-[32px] sm:leading-[40px]">My Work</h3>
             <div className="flex flex-row gap-x-[15px] mt-8 sm:mt-[56px] sm:gap-x-[30px]">
-                <img src={slides[index.previous] } className="w-[270px] h-[180px] overflow-visible ml-[-233px] rounded-xl sm:ml-[-495px] sm:w-[540px] sm:h-[360px]"/>
+                <img src={slides[index.previous] } className=" w-[270px] h-[180px] overflow-visible ml-[-249px] rounded-xl sm:ml-[-495px] sm:w-[540px] sm:h-[360px]"/>
                 <img src={slides[index.current]} className=" w-[270px] h-[180px] rounded-xl sm:w-[540px] sm:h-[360px]" />
-                <img src={slides[index.next]} className="w-[270px] h-[180px] overflow-visible mr-[-232px] sm:mr-[-495px] rounded-xl sm:w-[540px] sm:h-[360px]"/>
+                <img src={slides[index.next]} className="  w-[270px] h-[180px] overflow-visible mr-[-248px] sm:mr-[-495px] rounded-xl sm:w-[540px] sm:h-[360px]"/>
             </div>
             <div className="flex flex-row justify-between items-center gap-x-4  mt-8 sm:mt-[56px] mx-[115px]">
                 <button className=" bg-[#030303] w-[64px] h-[64px] flex justify-center items-center rounded-full hover:bg-[#755CDE] " onClick={previousHandler}>
